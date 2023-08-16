bd8819277acc03fb5155dcfad8d6eabc64c14a7b
Florian Mueller <florian.mueller@agilent.com>
2023-08-16 13:39:38 +0200

    resized buttons and gui

--- src/main/java/app/MainGui.java
+++ src/main/java/app/MainGui.java
@@ -7,65 +7,78 @@ import de.dhbwka.swe.utils.util.IPropertyManager;
 import javax.swing.*;
 import java.awt.*;
 
-class MainGui {
+public class MainGui {
     private JFrame frame;
-
 
     public MainGui() {
         this.frame = new JFrame();
-        this.frame.setSize(1000, 1000);
+        this.frame.setSize(1200, 600);
         this.frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        this.frame.setLayout(new FlowLayout(FlowLayout.CENTER));
+        this.frame.setLayout(new BorderLayout());
 
         renderFrame();
     }
 
     private void renderFrame() {
-        JPanel mainPanel = new JPanel(new GridLayout(3,1));
-        JPanel buttonPanel = new JPanel(new GridLayout(1,8));
+        JPanel mainPanel = new JPanel(new BorderLayout());
+
+        JPanel buttonPanel = createButtonPanel();
+        mainPanel.add(buttonPanel, BorderLayout.NORTH);
+
+        JPanel middlePanel = createMiddlePanel();
+        mainPanel.add(middlePanel, BorderLayout.CENTER);
+
+        this.frame.add(mainPanel);
+        this.frame.setVisible(true);
+    }
+
+    private JPanel createButtonPanel() {
+        JPanel buttonPanel = new JPanel(new FlowLayout(FlowLayout.CENTER, 20, 20));
+
+        String[] buttonLabels = {
+                "Buchungen", "Freie Plätze", "Neuer Platz",
+                "Platz bearbeiten", "Platz löschen", "Platz Buchen",
+                "Export/Import", "Login"
+        };
+
+        for (String label : buttonLabels) {
+            JButton button = new JButton(label);
+            button.setPreferredSize(new Dimension(120, 40)); // Button-Größe anpassen
+            buttonPanel.add(button);
+        }
+
+        return buttonPanel;
+    }
+
+    private JPanel createMiddlePanel() {
         JPanel middlePanel = new JPanel(new BorderLayout());
-        JPanel buchungsPanel = new JPanel(new GridLayout(1,1));
-        JPanel tablepanel = new JPanel();
 
-            buttonPanel.add(new JButton("Buchungen"));
-            buttonPanel.add(new JButton("Freie Plätze"));
-            buttonPanel.add(new JButton("Neuer Platz"));
-            buttonPanel.add(new JButton("Platz bearbeiten"));
-            buttonPanel.add(new JButton("Platz löschen"));
-            buttonPanel.add(new JButton("Platz Buchen"));
-            buttonPanel.add(new JButton("Export/Import"));
-            buttonPanel.add(new JButton("Login"));
+        ImageIcon imageIcon = new ImageIcon("./src/main/java/app/Platzplan.png");
+        JLabel imageLabel = new JLabel(imageIcon);
+        middlePanel.add(imageLabel, BorderLayout.WEST);
 
-
-            ImageIcon imageIcon = new ImageIcon("./src/main/java/app/Platzplan.png");
-            JLabel imageLabel = new JLabel(imageIcon);
-            middlePanel.add(imageLabel, BorderLayout.WEST);
         IPropertyManager propManager = null; // Falls du einen PropertyManager benötigst, kannst du ihn hier erstellen
-         // If you have a PropertyManager, you can pass it here
         SimpleTableComponentApp stcApp = new SimpleTableComponentApp();
         SimpleTableComponent simpleTableComponent = stcApp.createSimpleTableComponent("TableComponent", propManager);
         middlePanel.add(simpleTableComponent, BorderLayout.CENTER);
 
-        buchungsPanel.add(new JButton("Neue Buchung"));
-        buchungsPanel.add(new JButton("Buchung bearbeiten"));
-        buchungsPanel.add(new JButton("Buchung löschen"));
-        buchungsPanel.add(new JButton("Info"));
+        JPanel buchungsPanel = new JPanel(new FlowLayout(FlowLayout.CENTER, 20, 20));
+        String[] buchungsButtonLabels = {
+                "Neue Buchung", "Buchung bearbeiten",
+                "Buchung löschen", "Info"
+        };
 
-        SimpleTableComponentApp stcApp2 = new SimpleTableComponentApp();
-        SimpleTableComponent simpleTableComponent2 = stcApp2.createSimpleTableComponent("TableComponent", propManager);
-        tablepanel.add(simpleTableComponent2);
-
+        for (String label : buchungsButtonLabels) {
+            JButton button = new JButton(label);
+            button.setPreferredSize(new Dimension(140, 40)); // Button-Größe anpassen
+            buchungsPanel.add(button);
+        }
         middlePanel.add(buchungsPanel, BorderLayout.SOUTH);
 
-        mainPanel.add(buttonPanel);
-        mainPanel.add(middlePanel);
-        mainPanel.add(tablepanel);
-        this.frame.setContentPane(mainPanel);
-        this.frame.pack();
-        this.frame.setVisible(true);
-
+        return middlePanel;
     }
 
-
-
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(() -> new MainGui());
+    }
 }
