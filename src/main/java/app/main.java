ab2cf6782acf3e9e815c466b0ed49ee45f369495
Zamox <93971775+Zamox@users.noreply.github.com>
2023-08-16 15:58:01 +0200

    Main übernimmt Controller-Funktion
    D.H. beide GUIs werden vom Main geöffnet.

--- src/main/java/app/main.java
+++ src/main/java/app/main.java
@@ -1,11 +1,22 @@
 package app;
 
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
 public class main {
     public static void main(String[] args) {
         try {
             //IOUtilities.openInJFrame(new JButton("Hello Welt"), 200, 400, "Button", true);
             // use Calender Component App
-            new MainGui();
+            MainGui mainGUI = new MainGui();
+            mainGUI.loginBtn.addActionListener(new ActionListener() {
+                @Override
+                public void actionPerformed(ActionEvent e) {
+                    new Login(mainGUI);
+                }
+            });
+
 
 
         } catch (Exception e) {
