2923ca2982237bf6e5a6cb27fad0f219dc486a88
marius <marius.baier02@gmail.com>
2023-09-26 18:55:22 +0200

    Freie Plätze Button etc mit Funktion

--- src/main/java/app/upperLeftPlaetze.java
+++ src/main/java/app/upperLeftPlaetze.java
@@ -2,24 +2,16 @@ package app;
 
 import javax.swing.*;
 import java.awt.*;
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.IOException;
-import java.util.HashSet;
-import java.util.Set;
 
 public class upperLeftPlaetze {
     private JFrame frame;
 
     public upperLeftPlaetze() {
-
         this.frame = new JFrame("Platzregion Nord-West");
         this.frame.setSize(1000, 1000);
         this.frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
         this.frame.setLayout(new FlowLayout(FlowLayout.CENTER));
         renderFrame();
-        checkCSVAndColorButtons();
-
     }
 
     private void renderFrame() {
@@ -51,13 +43,12 @@ public class upperLeftPlaetze {
         mainPanel.add(column6Panel);
 
         // Spalte 7 mit 4 Buttons (individuelle Nummern von 50 bis 53)
-        JPanel column7Panel = createButtonPanel(2, 50);
+        JPanel column7Panel = createButtonPanel(4, 50);
         mainPanel.add(column7Panel);
 
         this.frame.setContentPane(mainPanel);
         this.frame.pack();
         this.frame.setVisible(true);
-        checkCSVAndColorButtons();
     }
 
     private JPanel createButtonPanel(int buttonCount, int startNumber) {
@@ -69,84 +60,6 @@ public class upperLeftPlaetze {
         }
 
         return panel;
-    }
-
-    private void checkCSVAndColorButtons() {
-        // Definieren Sie den Dateipfad Ihrer CSV-Datei
-        String csvFilePath = "./BuchungsCSV.csv"; // Ändern Sie dies entsprechend
-
-        // Holen Sie den Hauptpanel
-        Container mainPanel = this.frame.getContentPane();
-
-        // Erstellen Sie eine Menge (Set) für die in der CSV-Datei gefundenen Nummern
-        Set<String> foundNumbers = new HashSet<>();
-
-        // Durchlaufen Sie alle Komponenten im Hauptpanel (einschließlich verschachtelter Panels)
-        findButtonsAndCheckCSV(mainPanel, foundNumbers, csvFilePath);
-
-        // Durchlaufen Sie erneut alle Komponenten und färben Sie die Buttons entsprechend
-        colorButtons(mainPanel, foundNumbers);
-    }
-
-    private void findButtonsAndCheckCSV(Component component, Set<String> foundNumbers, String csvFilePath) {
-        if (component instanceof JButton) {
-            JButton button = (JButton) component;
-            String buttonText = button.getText().replace("Platz ", "");
-
-            if (checkCSVForNumber(buttonText, csvFilePath)) {
-                foundNumbers.add(buttonText);
-            }
-        } else if (component instanceof Container) {
-            // Wenn es sich um ein Container-Objekt handelt, durchlaufen Sie seine Komponenten
-            Container container = (Container) component;
-            Component[] components = container.getComponents();
-            for (Component subComponent : components) {
-                // Rekursiv in verschachtelten Containern nach Buttons suchen
-                findButtonsAndCheckCSV(subComponent, foundNumbers, csvFilePath);
-            }
-        }
-    }
-
-    private boolean checkCSVForNumber(String number, String csvFilePath) {
-        try (BufferedReader br = new BufferedReader(new FileReader(csvFilePath))) {
-            String line;
-            while ((line = br.readLine()) != null) {
-                String[] parts = line.split(","); // Annahme: CSV ist kommagetrennt
-
-                if (parts.length > 9) { // Annahme: Die 10. Spalte enthält die Zahlen
-                    String csvNumber = parts[4].trim(); // Ändern Sie den Index entsprechend
-                    if (csvNumber.equals(number)) {
-                        return true;
-                    }
-                }
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        return false;
-    }
-
-    private void colorButtons(Component component, Set<String> foundNumbers) {
-        if (component instanceof JButton) {
-            JButton button = (JButton) component;
-            String buttonText = button.getText().replace("Platz ", "");
-
-            if (foundNumbers.contains(buttonText)) {
-                // Die Nummer wurde in der CSV-Datei gefunden, färben Sie den Button rot
-                button.setBackground(Color.RED);
-            } else {
-                // Die Nummer wurde nicht in der CSV-Datei gefunden, färben Sie den Button grün
-                button.setBackground(Color.GREEN);
-            }
-        } else if (component instanceof Container) {
-            // Wenn es sich um ein Container-Objekt handelt, durchlaufen Sie seine Komponenten
-            Container container = (Container) component;
-            Component[] components = container.getComponents();
-            for (Component subComponent : components) {
-                // Rekursiv in verschachtelten Containern Buttons einfärben
-                colorButtons(subComponent, foundNumbers);
-            }
-        }
     }
 
 
