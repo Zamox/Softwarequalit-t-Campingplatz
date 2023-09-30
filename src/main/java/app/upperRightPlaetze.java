d830210c5637e33ea97efa8b9b8e69eef62cbeb2
Stack0verFlo <freeflomlxgmail.com>
2023-10-01 00:14:35 +0200

    upperRight Changes

--- src/main/java/app/upperRightPlaetze.java
+++ src/main/java/app/upperRightPlaetze.java
@@ -7,7 +7,6 @@ import java.awt.event.ActionListener;
 
 public class upperRightPlaetze {
 
-
     private JFrame frame;
 
     private JFrame parentframe;
@@ -19,7 +18,7 @@ public class upperRightPlaetze {
         this.frame = new JFrame("Platzregion Nord-Ost");
         this.frame.setSize(1000, 1000);
         this.frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-        this.frame.setLayout(new FlowLayout(FlowLayout.CENTER));
+        this.frame.setLayout(new GridLayout(1, 7));
         this.fall = fall;
         renderFrame();
     }
@@ -28,7 +27,6 @@ public class upperRightPlaetze {
         this.frame = new JFrame("Platzregion Nord-Ost");
         this.frame.setSize(1000, 1000);
         this.frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-        this.frame.setLayout(new FlowLayout(FlowLayout.CENTER));
         this.frame.setLayout(new GridLayout(1, 7));
         this.BuchungBearbeitenGui = BuchungBearbeitenGui;
         this.parentframe = parentframe;
@@ -40,7 +38,6 @@ public class upperRightPlaetze {
         this.frame = new JFrame("Platzregion Nord-Ost");
         this.frame.setSize(1000, 1000);
         this.frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-        this.frame.setLayout(new FlowLayout(FlowLayout.CENTER));
         this.frame.setLayout(new GridLayout(1, 7));
         this.BuchungErstellenGui = BuchungErstellenGui;
         this.parentframe = parentframe;
@@ -54,54 +51,11 @@ public class upperRightPlaetze {
         // Leerzeile für Spalte 2
         mainPanel.add(new JPanel());
 
-
         JPanel column1Panel = createButtonPanel(5, 56);
         mainPanel.add(column1Panel);
 
         // Leerzeile für Spalte 5
         mainPanel.add(new JPanel());
-
-        // Spalte 3 mit 4 Buttons (individuelle Nummern von 38 bis 41)
-        JPanel column3Panel = new JPanel(new GridLayout(4, 1));
-        JButton emptyButton1 = new JButton("");
-        column3Panel.add(emptyButton1);
-        JButton emptyButton2 = new JButton("");
-        JButton emptyButton3 = new JButton("");
-        JButton emptyButton4 = new JButton("");
-        column3Panel.add(emptyButton2);
-        column3Panel.add(emptyButton3);
-        column3Panel.add(emptyButton4);
-        emptyButton1.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                JOptionPane.showMessageDialog(frame, "Dieser Platz muss zunächst angelegt werden und kann noch nicht gebucht werden.");
-            }
-        });
-
-        emptyButton2.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                JOptionPane.showMessageDialog(frame, "Dieser Platz muss zunächst angelegt werden und kann noch nicht gebucht werden.");
-            }
-        });
-
-        emptyButton3.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                JOptionPane.showMessageDialog(frame, "Dieser Platz muss zunächst angelegt werden und kann noch nicht gebucht werden.");
-            }
-        });
-
-        emptyButton4.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                JOptionPane.showMessageDialog(frame, "Dieser Platz muss zunächst angelegt werden und kann noch nicht gebucht werden.");
-            }
-        });
-
-        mainPanel.add(column3Panel);
-
-        // Spalte 4 mit 4 Buttons (individuelle Nummern von 42 bis 45)
 
 
         // Leerzeile für Spalte 5
@@ -110,6 +64,9 @@ public class upperRightPlaetze {
         this.frame.setContentPane(mainPanel);
         this.frame.pack();
         this.frame.setVisible(true);
+
+        // Add click listener to buttons
+        addClickListenerToButtons(mainPanel);
     }
 
     private JPanel createButtonPanel(int buttonCount, int startNumber) {
@@ -123,7 +80,19 @@ public class upperRightPlaetze {
         return panel;
     }
 
+    private void addClickListenerToButtons(Container container) {
+        if (container instanceof JButton) {
+            JButton button = (JButton) container;
+            button.addActionListener(e -> {
+                // Handle button click event if needed
+            });
+
+        } else if (container instanceof Container) {
+            Container subContainer = (Container) container;
+            Component[] components = subContainer.getComponents();
+            for (Component component : components) {
+                addClickListenerToButtons((Container) component);
+            }
+        }
+    }
 }
-
-
-
